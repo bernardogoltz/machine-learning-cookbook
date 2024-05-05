cb7864952ec78626c784b7c7760eaccf39d1b54f
[Bernardo Goltz] <bergoltzx2@gmail.com>
2024-05-05 14:24:50 -0300

    mnist classifier

---
+++ classification/README.md
@@ -0,0 +1,5 @@
+# Classification
+- ```sklearn 1.3.0```
+- ```Python 3.11.5```
+
+
---
+++ classification/mnist_classifier.py
@@ -0,0 +1,6 @@
+import numpy
+import seaborn as sns 
+import numpy as np 
+import pandas as pd 
+
+from sklearn.datasets import fetch_openml
